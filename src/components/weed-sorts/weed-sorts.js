92f089f22aac33f58e302002913b822610e0251a
Stanislav Krasichinsky <thekrasich@gmail.com>
2022-11-22 23:07:58 +0200

    added weed-sorts

--- src/components/weed-sorts/weed-sorts.js
+++ src/components/weed-sorts/weed-sorts.js
@@ -1,16 +1,23 @@
-const WeedSortsPage = () =>{
 
-    const test = () => {
-        fetch('http://localhost:4000/weed-sort')
-        .then((response) => response.json())
-        .then((data) => console.log(data));
-    }
+let state;
+const test = async () => {
+  const result = await fetch("http://localhost:4000/weed-sort")
+    .then((response) => response.json())
+    .then((data) => (state = data));
+  console.log(result.recordset);
 
-    return(
-        <div>
-            <button onClick={test}>asdads</button>
-        </div>
-    )
-}
+  return result.recordset;
+};
+
+const WeedSortsPage = () => {
+  
+  console.log(state);
+  return (
+    <div>
+      <button onClick={test}>asdads</button>
+      <ul>{}</ul>
+    </div>
+  );
+};
 
 export default WeedSortsPage;
